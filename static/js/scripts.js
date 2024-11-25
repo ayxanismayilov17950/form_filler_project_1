ad57725dbd0d2c5ba02cf3a59301af73ed612f43
aismayilov17950 <aismayilov17950@ada.edu.az>
2024-11-25 17:55:15 +0400

    commit

--- static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector(".header");
-const navbarLogo = document.querySelector(".navbar__logo");
-// navbar.offsetTop won't work when the user reloads the page when navbar is sticked
-const navPos = header.offsetHeight;
-
-window.onscroll = () => {
-  navbarLogo.classList.toggle("navbar__logo--visible", navPos < window.scrollY);
-};
